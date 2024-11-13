ef24e23ea91c85591b5c99c207fad22369682890
tadeasf <business@tadeasfort.com>
2024-11-13 10:07:10 +0100

    Add Mongoose modules for ThreadsAuth and ThreadsInsights schemas

--- src/threads/threads.module.ts
+++ src/threads/threads.module.ts
@@ -3,9 +3,24 @@ import { ThreadsService } from './threads.service';
 import { ThreadsController } from './threads.controller';
 import { GraphQLClient } from './graphql.client';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  ThreadsAuth,
+  ThreadsAuthSchema,
+} from '../auth/schemas/threads-auth.schema';
+import {
+  ThreadsInsights,
+  ThreadsInsightsSchema,
+} from './schemas/threads-insights.schema';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forFeature([
+      { name: ThreadsAuth.name, schema: ThreadsAuthSchema },
+      { name: ThreadsInsights.name, schema: ThreadsInsightsSchema },
+    ]),
+  ],
   providers: [ThreadsService, GraphQLClient],
   controllers: [ThreadsController],
 })
