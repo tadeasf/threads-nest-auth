07fcf9e68bcbf51a14b43c007a04a2a3d37607db
tadeasf <business@tadeasfort.com>
2024-11-23 21:56:06 +0100

    refactor(app.module.ts): remove deprecated MongoDB connection options useNewUrlParser and useUnifiedTopology to clean up the code and align with the latest MongoDB driver practices

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,8 +24,6 @@ import RedisStore from 'connect-redis';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
       }),
       inject: [ConfigService],
     }),
